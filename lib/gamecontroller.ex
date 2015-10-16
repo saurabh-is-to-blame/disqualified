4342e242e253019020e6c8023f0866e638a6f992
Saurabh Kumar <saurabh.kumar@housing.com>
2015-10-17 03:04:42 +0530

    Change topic names to match stupid android client

--- lib/gamecontroller.ex
+++ lib/gamecontroller.ex
@@ -8,9 +8,9 @@ defmodule Disqualified.GameController do
 
   def init(_opts) do
     {:ok, client} = Mqtt.connect
-    :ok = Mqtt.subscribe(client, "/+/init", 1)
-    :ok = Mqtt.subscribe(client, "/+/register", 1)
-    :ok = Mqtt.subscribe(client, "/+/results", 1)
+    :ok = Mqtt.subscribe(client, "/chat/+/init", 1)
+    :ok = Mqtt.subscribe(client, "/chat/+/register", 1)
+    :ok = Mqtt.subscribe(client, "/chat/+/results", 1)
     {:ok, %{client: client}}
   end
 
@@ -35,7 +35,7 @@ defmodule Disqualified.GameController do
   end
 
   def mqtt_receive(channel, payload, client) do
-    [ _, game_id, values] = String.split(channel, "/")
+    [ _, _, game_id, values] = String.split(channel, "/")
     case values do
       "init" ->
         Logger.debug "Game started. Game Id: " <> game_id
@@ -53,10 +53,10 @@ defmodule Disqualified.GameController do
     {:ok, numbers} =
       Enum.map(list, fn(x) -> convert_to_integer(x) end)
     |> JSON.encode
-    Mqtt.publish(client, "/" <> game_id <> "/fetch", numbers, 1)
+    Mqtt.publish(client, "/" <> game_id <> "/chat/fetch", numbers, 1)
   end
 
   defp convert_to_integer(x) do
-    Float.round(x*2)
+    round(x*2)
   end
 end
